885efe91f5f6e972d73d92bd9bbe556fdfdd31c5
Robert Druska <robert.druska@gmail.com>
2024-10-20 17:55:10 +0200

    get shared fallback on agenda jobs

--- server/api/get-shared.js
+++ server/api/get-shared.js
@@ -3,9 +3,21 @@ import { ObjectId } from 'mongodb';
 
 import client from '../db/mongo-client';
 
+const getResult = async (id, dbName, collectionName) => {
+  const database = client.db(dbName);
+  const collection = database.collection(collectionName);
+
+  const result = await collection.findOne({ _id: new ObjectId(id) });
+
+  return result;
+}
+
 export default defineEventHandler(async (event) => {
+  console.log('get-shared')
   const query = getQuery(event);
   const id = query.id;
+
+  console.log(`id ${id}`)
 
   if (!id) {
     return createError({
@@ -15,10 +27,19 @@ export default defineEventHandler(async (event) => {
   }
 
   try {
-    const database = client.db('bookCharacters');
-    const collection = database.collection('characters');
+    let result = await getResult(id, 'bookCharacters', 'characters');
+    console.log(`result ${id}`, result)
 
-    const result = await collection.findOne({ _id: new ObjectId(id) });
+    if (!result) {
+      result = await getResult(id, process.env.MONGODB_DB_NAME, 'agendaJobs');
+      console.log(`result ${process.env.MONGODB_DB_NAME} ${id}`, result)
+      if (result) {
+        return {
+          bookName: result.data.filename,
+          characters: result.data.characters
+        };
+      }
+    }
 
     if (!result) {
       return createError({
